86b40efd9226663a47ecf2a0c456060deb43463d
Khess S Dotto <khessdotto@gmail.com>
2023-02-20 15:02:27 -0300

    Added 'app.use(express.json())' to deal with json/js conversions and added 'app.listen' to activate localhost port 4000.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import express from "express";
 import { v4 as uuid } from "uuid";
+
+const app = express();
+app.use(express.json());
+
+app.listen(4000, () => {
+  console.log("Server up and running on port 4000.");
+});
